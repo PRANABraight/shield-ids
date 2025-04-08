6e669252a683ed784670473e0d4356f8d0e97d0d
Pranab Rai <pranab.rai@mca.christuniversity.in>
2025-04-08 09:59:45 +0530

    dependency fixes

--- shieldids/src/vite-env.d.ts
+++ shieldids/src/vite-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="vite/client" />
+
+// Declare react-icons modules to ensure they are properly recognized
+declare module 'react-icons/fa' {
+  export * from '@react-icons/all-files/fa';
+}
+
+declare module 'react-icons/fi' {
+  export * from '@react-icons/all-files/fi';
+}
+
+declare module 'react-icons/*' {
+  const content: any;
+  export default content;
+}
